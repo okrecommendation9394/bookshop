bb0a801925079fe9b75338bb5b1cccab8813acc5
Ketevan Gogia <91284247+okrecommendation9394@users.noreply.github.com>
2022-05-24 13:43:02 -0700

    feat: changes

--- books.js
+++ books.js
@@ -1,63 +1,90 @@
-"use strict";
-/*let shopName=document.createElement('div');
-shopName.innerHTML='BookShop';
-document.getElementById('name').appendChild(shopName);*/
-for(let i=0; i<10; i++){
-  let buttons='';
-  let btn= document.createElement("button");
-  btn.type='button';
-  btn.name='learn';
-  btn.id="moreBtn";
-  btn.onclick = function(){
-    alert(arr[i]['info']);
-  }
-  btn.innerHTML="Learn More";
-  document.getElementById('myButtons').appendChild(btn);
-};
-let arr=[];
-document.getElementById('moreBtn').addEventListener('click', $.getJSON());
-$.getJSON('books.json', function(json){
-  //let arr=[];
-  for(let key in json){
-    if(json.hasOwnProperty(key)){
-      let item = json[key];
-      arr.push({
-        'info':item['description']
-      });
-    }
-
-  }
- // console.log(arr);
-});
-console.log(arr);
-function CustumAlert(){
-  this.render = function(){
-
-  }
-  this.ok = function(){
-
-  }
+'use strict';
+let header=document.createElement('div');
+header.id='header';
+let heading=document.createElement('h1');
+heading.innerHTML='BookShop';
+let orderBtn=document.createElement('button');
+orderBtn.innerHTML='Order';
+orderBtn.id='order';
+let price=document.createElement('div');
+//price.innerHTML=`Total: ${sum} Dollars`
+//price.id='price';
+document.getElementById('draggable').appendChild(header);
+document.getElementById('header').appendChild(heading);
+document.getElementById('header').appendChild(orderBtn);
+//document.getElementById('header').appendChild(price);
+orderBtn.onclick=function(){
+    // let form=document.createElement()
 }
-let Alert=new CustumAlert();
-const draggables=document.querySelectorAll('.each');
-const containers=document.querySelectorAll('.dragBox');
-console.log(containers);
+let arr=[];
+$.getJSON('books.json', function(json){
+    for(let key in json){
+      if(json.hasOwnProperty(key)){
+        let item = json[key];
+        arr.push({
+          'image':item['imageLink'],
+          'name':item['title'],
+          'author':item['author'],
+          'price': item['price'],
+          'description':item['description']
+        });
+      }
+  
+    }
+    let sum=0;
+    let price=document.createElement('div');
+    price.id='price';
+    document.getElementById('draggable').appendChild(price);
+    price.innerHTML=`Total: ${sum} Dollars`
+    for(let i=0; i<10; i++){
+        let elem=document.createElement('div');
+        elem.classList.add('each');
+        elem.innerHTML=`
+        <div>${arr[i]['name']}</div>
+        <div>${arr[i]['author']}</div>
+        <div>${arr[i]['price']} Dollars</div>
+        <img src=${arr[i]['image']}>
+        `
+        let btn=document.createElement('button');
+        btn.innerHTML='Show more';
+        btn.id='mybtn'
+        btn.onclick=function(){
+            let overlay=document.createElement('div');
+            overlay.id='overlay';
+            let pop=document.createElement('div');
+            pop.id='mainpop';
+            let summery=document.createElement('div');
+            summery.id='popup';
+            summery.innerHTML=arr[i]['description'];
+            let closeBtn=document.createElement('button')
+            closeBtn.innerHTML='&times';
+            closeBtn.id='close_button';
+            document.getElementById('draggable').appendChild(pop);
+            document.getElementById('mainpop').appendChild(closeBtn);
+            document.getElementById('mainpop').appendChild(summery);
+            document.getElementById('wrapper').appendChild(overlay);
+            closeBtn.onclick=function(){
+                pop.remove();
+                overlay.remove();
+            }
+        }
 
-draggables.forEach(draggable =>{
-  draggable.addEventListener('dragstart', () => {
-    console.log('aaaa');
-    draggable.classList.add('dragging')
-  })
-  draggable.addEventListener('dragend', ()=>{
-    draggable.classList.remove('dragging')
-  })
-})
+        let add=document.createElement('button');
+        add.innerHTML='Add to bag';
+        add.id='add';
+        add.onclick=function(){
+            sum+=arr[i]['price'];
+            price.innerHTML=`Total: ${sum} Dollars`;
+            elem.classList.add('dragged');
+        }
 
-containers.forEach(container => {
-  container.addEventListener('dragover', e=>{
-    console.log('drag over');
-    e.preventDefault();
-    const draggable=document.querySelector('.dragging');
-    container.appendChild(draggable);
-  })
-})
+
+        document.getElementById('draggable').appendChild(elem);
+        document.getElementById('draggable').appendChild(btn);
+        document.getElementById('draggable').appendChild(add);
+
+    }
+    let bag=document.createElement('div');
+    bag.id='cart';
+    document.getElementById('wrapper').appendChild(bag);
+  });
